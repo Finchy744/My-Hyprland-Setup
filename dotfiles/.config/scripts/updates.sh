080de5b1274aed68a948271b7293bd5de4238c62
Finchy744 <jordanfincher30@yahoo.com>
2026-02-08 03:36:19 -0600

    delete

--- dotfiles/.config/scripts/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-updates=$(checkupdates 2>/dev/null | wc -l)
-if [[ "$updates" -gt 0 ]]; then
-  echo "{\"text\": \"⬆️ $updates updates\", \"tooltip\": \"You have $updates updates pending.\"}"
-else
-  echo "{\"text\": \"✅ Up to date\", \"tooltip\": \"Your system is fully updated.\"}"
-fi
